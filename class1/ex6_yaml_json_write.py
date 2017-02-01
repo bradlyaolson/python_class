6de738f58b7416cdf57354ca835da742dd616295
Bradly.Olson <z001955@c4b301c71fcb.hq.target.com>
2017-02-01 13:12:43 -0600

    ex6 test

--- class1/ex6_yaml_json_write.py
+++ class1/ex6_yaml_json_write.py
@@ -1,47 +1,31 @@
 #!/usr/bin/env python
-'''
-Write a Python program that creates a list. One of the elements of the list
-should be a dictionary with at least two keys. Write this list out to a file
-using both YAML and JSON formats. The YAML file should be in the expanded form.
-'''
-
 import yaml
 import json
 
+def dumps():
+    yaml_file = 'ex6_test.yml'
+    json_file = 'ex6_test.json'
 
-def main():
-    '''
-    Write a Python program that creates a list. One of the elements of the list
-    should be a dictionary with at least two keys. Write this list out to a file
-    using both YAML and JSON formats. The YAML file should be in the expanded
-    form.
-    '''
-
-    yaml_file = 'my_test.yml'
-    json_file = 'my_test.json'
-
-    my_dict = {
-        'ip_addr': '172.31.200.1',
-        'platform': 'cisco_ios',
-        'vendor':   'cisco',
-        'model':    '1921'
+    dict_1 = {
+        'ip_addr': '10.10.10.10',
+        'Device_Type': 'Router',
+        'Platform': 'DC',
+        'model':    '2951'
     }
 
-    my_list = [
-        'some string',
-        99,
-        18,
-        my_dict,
-        'another string',
-        'final string'
-    ]
+    list_1 = {
+        'confreg',
+        'uptime',
+        'dir',
+        dict1
+    }
 
-    with open(yaml_file, "w") as f:
-        f.write(yaml.dump(my_list, default_flow_style=False))
+    with open("yaml_file", "w") as f:
+        f.write(yaml.dump(list_1, default_flow_style=False))
 
     with open(json_file, "w") as f:
-        json.dump(my_list, f)
+        json.dump(list_1, f)
 
-
-if __name__ == "__main__":
-    main()
+#
+# if __name__ == "__main__":
+#     main()
